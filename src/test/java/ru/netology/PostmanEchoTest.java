c19d929e5554775b0ec498dd195c547dc4f12d77
Vladimir Kolyannikov <kvazitron111@gmail.com>
2022-09-09 11:05:36 +0300

    first commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPost() {
+
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("ПРОВЕРКА") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("ПРОВЕРКА"))
+
+//                .body("",equalTo("some data"))
+//                .body("data", equalTo("1"))
+
+
+        ;
+    }
+}
